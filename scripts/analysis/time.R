0bfca6f8a2f1fc400e485e1f7d35749c0c03a8ad
mischko <mhem@mailbox.org>
2024-01-06 11:04:07 +0100

    remove unnecessary parts of the time analysis

--- scripts/analysis/time.R
+++ scripts/analysis/time.R
@@ -29,14 +29,6 @@ vars_time <-
     select(granulos_CSF:lactate_CSF) |>
     names()
 
-TimePlot(combined_complete_time_somatoform, "granulos_CSF", size = 0.1, span = 0.2)
-
-# all data
-time_plots_all <- lapply(vars_time, TimePlot, data = combined_complete_time, size = 0.1, span = 0.2)
-time_plots_all_patch <- patchwork::wrap_plots(time_plots_all, ncol = 4)
-ggsave(file.path("analysis", "relative", "time", "time_all.png"), plot = time_plots_all_patch, width = 15, height = 50, limitsize = FALSE)
-
-# only somatoform
 combined_complete_time_somatoform <- 
     combined_complete_time |>
     dplyr::filter(dx_icd_level2 == "somatoform")
