49755309fe57102b9da3616b0c7a42d1bcc5a1a7
mischko <mhem@mailbox.org>
2024-09-21 09:56:07 +0200

    analyze np data

--- scripts/analysis/umap_cluster_enrichment.R
+++ scripts/analysis/umap_cluster_enrichment.R
@@ -165,42 +165,57 @@ ggsave(
     height = 6
 )
 
-# read in neuropsychology data ---
+# analyze neuropsychological data ----
+# based on manual compiled data
+patients_cluster_dementia_manual_merge <- qs::qread("patients_cluster_dementia_manual_merge.qs")
+
+boxplot_dementia_cluster_manual <- function(test_name) {
+formula <- paste0(test_name, "~", "cluster")
+patients_dementia_manual_merge_stat <-
+    patients_cluster_dementia_manual_merge |>
+    dplyr::mutate(cluster = if_else(cluster == "neurodegenerative", "neurodegenerative", "other")) |>
+    wilcox.test(as.formula(formula), data = _) |>
+    broom::tidy() |>
+    mutate(p.symbol = as.character(symnum(p.value, corr = FALSE, na = FALSE, cutpoints = c(0, 0.001, 0.01, 0.05, 1), symbols = c("***", "**", "*", ""))))
+
+stats_list <- vector("list")
+stats_list$annotation <- patients_dementia_manual_merge_stat$p.symbol
+stats_list$comparisons[[1]] <- c("neurodegenerative", "other")
+
+patients_dementia_manual_merge_plot <-
+    patients_cluster_dementia_manual_merge |>
+    dplyr::mutate(cluster = if_else(cluster == "neurodegenerative", "neurodegenerative", "other")) |>
+    ggplot(aes(x = cluster, y = .data[[test_name]], fill = cluster)) +
+    geom_boxplot() +
+    geom_jitter(width = 0.3, height = 0, size = .7, shape = 21, aes(fill = cluster)) +
+    theme_bw() +
+    xlab("") +
+    ylab(test_name) +
+    theme(legend.position = "none") +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) 
+
+ggsave(
+    patients_dementia_manual_merge_plot,
+    filename = file.path("analysis", "relative", "boxplots", paste0("patients_dementia_manual_", test_name, ".pdf")),
+    width = 2,
+    height = 5
+)
+}
+
+lapply(
+    c("MMSE", "MoCA"),
+    boxplot_dementia_cluster_manual
+)
+# load extraced neuropsychological data
+# contains all available neuropsychological data of the patients (all dates)
 np_dementia <- qs::qread("np_dementia.qs")
 
-
+# join patients with neuropsychology data ----
 dementia_patients <-
     combined_complete_norm |>
     mutate(cluster = seu_csf_train$cluster) |>
     dplyr::filter(dx_icd_level2 == "dementia")  |>
     select(pid, cluster, measure_date)
-
-# join patients with neuropsychology data ----
-top_np_item <-
-    np_dementia |>
-    count(test) |>
-    slice_max(n) |>
-    pull(test)
-
-# only the item with the most complete data
-# take the nearest date
-# remove if measure date and test date are more than 30 days apart
-np_dementia_enrich_v1 <-
-    np_dementia |>
-    dplyr::filter(test %in% top_np_item) |>
-    inner_join(dementia_patients, by = join_by(pid)) |>
-    group_by(pid) |>
-    fuzzyjoin::fuzzy_inner_join(dementia_patients,
-        by = c("pid", "test_date" = "measure_date"),
-        match_fun = list(`==`, date_distance_fun)
-    ) |>
-    ungroup() |>
-    select(!c(pid.y, measure_date.y)) |>
-    rename(
-        pid = pid.x,
-        measure_date = measure_date.x
-    ) |>
-    mutate(interval = as.double(difftime(measure_date, test_date, units = "days")))
 
 # function to calculate distance between dates
 date_distance_fun <- function(v1, v2, max_dist = 1) {
@@ -209,10 +224,64 @@ date_distance_fun <- function(v1, v2, max_dist = 1) {
     return(ret)
 }
 
-orbis_pseudo_all_timm_1 <- 
-    orbis_pseudo_match_all |>
-    fuzzyjoin::fuzzy_inner_join(timm_combined,
-                                by = c("pid", "measure_date_orbis" = "measure_date"),
-                                match_fun = list(`==`, date_distance_fun)) |>
-    select(-pid.y) |>
-    rename(pid = pid.x)
+# function to plot NPU scores in neurodegenerative vs other clusters
+boxplot_dementia_cluster_np <- function(test_name) {
+    # use the nearest date
+    # remove if measure date and test date are more than 30 days apart
+    np_dementia_df <-
+        np_dementia |>
+        dplyr::filter(test %in% test_name) |>
+        fuzzyjoin::fuzzy_inner_join(dementia_patients,
+            by = c("pid", "test_date" = "measure_date"),
+            match_fun = list(`==`, date_distance_fun)
+        ) |>
+        select(!c(pid.y)) |>
+        rename(pid = pid.x) |>
+        mutate(interval = as.double(difftime(measure_date, test_date, units = "days"))) |>
+        dplyr::filter(interval <= 30)
+
+    np_dementia_stat <-
+        np_dementia_df |>
+        dplyr::mutate(cluster = if_else(cluster == "neurodegenerative", "neurodegenerative", "other")) |>
+        wilcox.test(formula = score_abs ~ cluster, data = _) |>
+        broom::tidy() |>
+        mutate(p.symbol = as.character(symnum(p.value, corr = FALSE, na = FALSE, cutpoints = c(0, 0.001, 0.01, 0.05, 1), symbols = c("***", "**", "*", ""))))
+
+    stats_list <- vector("list")
+        stats_list$annotation <- np_dementia_stat$p.symbol
+        stats_list$comparisons[[1]] <- c("neurodegenerative", "other")
+
+    np_dementia_plot <-
+        np_dementia_df |>
+        dplyr::mutate(cluster = if_else(cluster == "neurodegenerative", "neurodegenerative", "other")) |>
+        ggplot(aes(x = cluster, y = score_abs, fill = cluster)) +
+        geom_boxplot() +
+        geom_jitter(width = 0.3, height = 0, size = .7, shape = 21, aes(fill = cluster)) +
+        theme_bw() +
+        xlab("") +
+        ylab("MMSE") +
+        theme(legend.position = "none") +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+        if (np_dementia_stat$p.value < 0.05) {
+            ggsignif::geom_signif(comparisons = stats_list$comparisons, annotation = stats_list$annotation, textsize = 5, step_increase = 0.05, vjust = 0.7)
+        }
+
+    ggsave(
+        filename = file.path("analysis", "relative", "boxplots", paste0("patients_dementia_np_", test_name, "_plot.pdf")),
+        np_dementia_plot,
+        width = 2,
+        height = 5
+    )
+}
+lapply(
+    c("MMSE", "MoCA"),
+    boxplot_dementia_cluster_np
+)
+
+test_top <-
+    np_dementia |>
+    count(test, sort = TRUE) |>
+    slice_max(n = 30, order_by = n) |>
+    pull(test)
+
+lapply(test_top, boxplot_enrich_dementia)
