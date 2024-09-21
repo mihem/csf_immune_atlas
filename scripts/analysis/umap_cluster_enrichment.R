360799a7dbb0c99d1dc90c8e7b5eacdcc61368f4
mischko <mhem@mailbox.org>
2024-09-21 13:28:54 +0200

    np volcano and np longitudinal

--- scripts/analysis/umap_cluster_enrichment.R
+++ scripts/analysis/umap_cluster_enrichment.R
@@ -7,12 +7,13 @@ library(tidyverse)
 library(readxl)
 library(fuzzyjoin)
 library(qs)
+library(RColorBrewer)
+library(Polychrome)
 
 # read in custom functions ----
 source("scripts/analysis/ml_izkf_utils.R")
 
-set.seed(123)
-my_cols <- unname(Polychrome::createPalette(100, pals::cols25()))
+my_cols <- unname(Polychrome::createPalette(300, RColorBrewer::brewer.pal(8, "Set2")))
 
 # read in data ---
 seu_csf_train <- qs::qread("seu_csf_train.qs")
@@ -259,7 +260,7 @@ boxplot_dementia_cluster_np <- function(test_name) {
         geom_jitter(width = 0.3, height = 0, size = .7, shape = 21, aes(fill = cluster)) +
         theme_bw() +
         xlab("") +
-        ylab("MMSE") +
+        ylab(test_name) +
         theme(legend.position = "none") +
         theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
         if (np_dementia_stat$p.value < 0.05) {
@@ -273,15 +274,128 @@ boxplot_dementia_cluster_np <- function(test_name) {
         height = 5
     )
 }
+
 lapply(
     c("MMSE", "MoCA"),
     boxplot_dementia_cluster_np
 )
 
-test_top <-
-    np_dementia |>
-    count(test, sort = TRUE) |>
-    slice_max(n = 30, order_by = n) |>
-    pull(test)
 
-lapply(test_top, boxplot_enrich_dementia)
+# function to calcate statistics for all np tests
+# exluce samples with less than 30 patients
+volcano_stat_dementia_np <- function(test_name) {
+    # use the nearest date
+    # remove if measure date and test date are more than 30 days apart
+    np_dementia_df <-
+        np_dementia |>
+        dplyr::filter(test %in% test_name) |>
+        fuzzyjoin::fuzzy_inner_join(dementia_patients,
+            by = c("pid", "test_date" = "measure_date"),
+            match_fun = list(`==`, date_distance_fun)
+        ) |>
+        select(!c(pid.y)) |>
+        rename(pid = pid.x) |>
+        mutate(interval = as.double(difftime(measure_date, test_date, units = "days"))) |>
+        dplyr::filter(interval <= 30)
+
+stopifnot(nrow(np_dementia_df) > 30)
+
+    np_dementia_stat <-
+        np_dementia_df |>
+        dplyr::mutate(cluster = if_else(cluster == "neurodegenerative", "neurodegenerative", "other")) |>
+        wilcox.test(formula = score_abs ~ cluster, data = _) |> 
+        broom::tidy() 
+
+   akp_effect <- WRS2::akp.effect(score_abs ~ cluster, data = np_dementia_df)
+    result <- tibble(
+        test = test_name,
+        p_value = np_dementia_stat$p.value,
+        effect = akp_effect$AKPeffect
+    )
+    return(result)
+}
+
+# calculate statistics
+results_volcano_stat_dementia <-
+    purrr::map_dfr(
+        unique(np_dementia$test),
+        possibly(volcano_stat_dementia_np, otherwise = NULL)) |>
+    mutate(p_adjust = p.adjust(p_value, method = "BH")) |>
+    mutate(neg_log10_p_adjust = -log10(p_adjust)) |>
+    mutate(sig = ifelse(neg_log10_p_adjust > -log10(0.05) & abs(effect) > 0.5, "yes", "no")) |>
+    mutate(var = ifelse(sig == "yes", test, ""))
+
+
+# volcano plot
+volcano_plot_dementia <-
+    results_volcano_stat_dementia |>
+    ggplot(aes(x = effect, y = neg_log10_p_adjust, label = var, color = sig)) +
+    geom_point() +
+    ggrepel::geom_text_repel() +
+    geom_hline(yintercept = -log10(0.05), linetype = "dashed") + # horizontal line p unadjusted
+    geom_vline(xintercept = -0.5, linetype = "dashed") + # vertical line
+    geom_vline(xintercept = 0.5, linetype = "dashed") + # vertical line
+    xlab("effect size") +
+    ylab(bquote(-Log[10] ~ "adjusted p value")) +
+    theme(legend.position = "none") +
+    theme_bw() +
+    guides(color = "none") +
+    scale_color_manual(values = c("yes" = RColorBrewer::brewer.pal(3, "Set1")[1], "no" = "black"))
+
+ggsave(
+    filename = file.path("analysis", "relative", "abundance", "volcano_plot_dementia.pdf"),
+    volcano_plot_dementia,
+    width = 5,
+    height = 5)
+
+results_volcano_stat_dementia |>
+    dplyr::arrange(desc(abs(effect))) 
+
+# also no significant adjusted p values for pr (procent rank) using wilcox and range (categorized) using fisher
+
+# longitudinal np data
+np_dementia_longitudinal_mmse <-
+    np_dementia |>
+    dplyr::filter(test %in% "MMSE") |>
+    inner_join(dementia_patients, by = join_by(pid)) |>
+    mutate(interval = time_length(interval(measure_date, test_date), unit = "months")) |>
+    mutate(cluster = if_else(cluster == "neurodegenerative", "neurodegenerative", "other")) |>
+    mutate(pid = as.character(pid)) |>
+    group_by(pid) |>
+    dplyr::filter(n() > 1) |>
+    ungroup() |>
+    mutate(interval_month = floor(interval))
+
+
+line_plot_dementia_longitudinal_mmse <- function(data, cluster_selected) {
+    # only keep samles with rounded intervals with more than 2 values
+interval_counts <- data |>
+        dplyr::group_by(interval_month) |>
+        dplyr::summarise(count = n()) |>
+        dplyr::filter(count > 2) 
+
+    filtered_data <- data |>
+        dplyr::filter(interval_month %in% interval_counts$interval_month)
+
+    plot <-
+        filtered_data |>
+        dplyr::filter(cluster %in% c(cluster_selected)) |>
+        ggplot(aes(x = interval, y = score_abs)) +
+        geom_point(aes(color = pid)) +
+        geom_line(aes(color = pid)) +
+        geom_smooth(method = "loess") +
+        theme_bw() +
+        xlab("") +
+        ylab("MMSE") +
+        theme(legend.position = "none") +
+        # xlim(0, 30) +
+        ylim(0, 30) +
+        scale_color_manual(values = my_cols)
+    ggsave(file.path("analysis", "relative", "abundance", paste0("longitudinal_", cluster_selected, "_mmse.pdf")),
+        plot,
+        width = 5, height = 5
+    )
+}
+
+line_plot_dementia_longitudinal_mmse(data = np_dementia_longitudinal_mmse, cluster_selected = "neurodegenerative")
+line_plot_dementia_longitudinal_mmse(data = np_dementia_longitudinal_mmse, cluster_selected = "other")
